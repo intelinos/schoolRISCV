04ace6b608ab39f1851405dd10060e9aee558dc0
Константин Строзенко <128403757+intelinos@users.noreply.github.com>
2025-01-18 11:45:11 +0300

    final

--- src/fifo.v
+++ src/fifo.v
@@ -1,53 +1,54 @@
 
 module fifo
-#(
-    parameter DATA_WIDTH = 32,
-              ADDR_WIDTH = 3
-)
 (
     input clk,
     input reset,
     input push,
-    input [DATA_WIDTH-1:0] in,
+    input [31:0] in,
     input pop,
-    output reg [DATA_WIDTH-1:0] out
+    output reg [31:0] out
 );
-    parameter DEPTH = 2 ** ADDR_WIDTH;
     
-    reg [DATA_WIDTH-1:0] fifo_mem [DEPTH-1:0];
-    reg [ADDR_WIDTH-1:0] write_ptr = 0;
-    reg [ADDR_WIDTH-1:0] read_ptr = 0;
+    reg [31:0] fifo_mem [7:0];
+    reg [2:0] write_ptr = 0;
+    reg [2:0] read_ptr = 0;
     reg full = 0;
     reg empty = 1;
-    
+    reg [3:0] count = 0;
+
     always @(negedge reset) begin
         write_ptr <= 0;
         read_ptr <= 0;
         out <= 0;
         full <= 0;
         empty <= 1;
+        count <= 0;
     end
 
     always @(posedge clk) begin
         if (push && !pop && !full) begin
             fifo_mem[write_ptr] <= in;
-            write_ptr <= (write_ptr + 1) % DEPTH;
+            write_ptr <= (write_ptr + 1) % 8;
             empty <= 0;
-            
-            if ((write_ptr + 1) % DEPTH == read_ptr) begin
+            count <= count + 1;
+
+            if (count == 8) begin
                 full <= 1;
             end
         end
     end
 
 
+
+
     always @(negedge clk) begin
         if (pop && !push && !empty) begin
             out <= fifo_mem[read_ptr];
-            read_ptr <= (read_ptr + 1) % DEPTH;
+            read_ptr <= (read_ptr + 1) % 8;
             full <= 0;
-            
-            if ((read_ptr + 1) % DEPTH == write_ptr) begin
+            count <= count - 1;
+
+            if (count == 0) begin
                 empty <= 1;
             end
         end
